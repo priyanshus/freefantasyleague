164639f2ab57764cf50e3ffe629d759818d8e8ca
Priyanshu Shekhar <priyanshushekhar@Priyanshus-MacBook---EE.local>
2025-04-01 19:57:37 +0530

    Update server.js

--- bff/server.js
+++ bff/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 
 const POLL_FILE = "polls.json";
